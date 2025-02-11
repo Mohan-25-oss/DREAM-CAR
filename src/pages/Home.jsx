df82a2dd1d1420044878ee30799dbd4e17e51c6b
Mohan <bagan.halder@yahoo.com>
2025-02-11 18:18:19 +0800

    Initial Step is complited

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Hero from '../components/Hero';
+
+const Home = () => {
+    return (
+        <>
+            <Header></Header>
+            <Hero></Hero>
+            <Footer></Footer>
+        </>
+    );
+};
+
+export default Home;
